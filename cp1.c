555158cc4bd3a611561f174a735807ee59629223
BrunaDamaris <38295264+BrunaDamaris@users.noreply.github.com>
2018-05-22 11:52:39 -0300

    Update cp1.c

--- cp1.c
+++ cp1.c
@@ -20,52 +20,168 @@
 GtkWidget *view;
 GtkTextBuffer *saida;
 GtkTextIter inicio,fim;
-int meusocket;
+GtkWidget *user;
+char *u;
+char msgant[4096];
+int meusocket,l=0;
 struct sockaddr_in servaddr;
 
-
-void str_cli(FILE *fp, int meusocket,GtkWidget *widget,GtkWidget *entrada)
+char *converttostring(const char *s) 
 {
-	char msgreceb[4096]; // dois chars
-	int i=0;
-	int lcount=-1;
-  	const gchar *mensagem,*msg;
- 	
-	mensagem = gtk_entry_get_text(GTK_ENTRY(entrada));
-	printf("Mensagem enviada: %s\n", mensagem);
-	write(meusocket, mensagem, strlen(mensagem));
+    char *m = malloc (strlen (s) + 1);   // Space for length plus nul
+    if (m == NULL) return NULL;          // No memory
+    strcpy (m,s);                       // Copy the characters
+    return m;                            // Return the new string
+}
 
-	printf("aguardando mensagem ...\n");
+void str_cli(FILE *fp,int meusocket,GtkWidget *widget,GtkWidget *entrada)
+{
+	char coiso[4096],newmsgfinal[4056],newmsg[4096],msgfinal[4096],msgenviada[4096], msgreceb[4096], user1[4096],msg[4096]; // dois chars
+	int i=0,j=0,ult;
+	int p=0;
+  	char *msg1;
+  	const char *mensagem;
+  	long int lcount=0;
+  	gsize bytes_read = 0;
+  	gsize bytes_written = 0;
+	GError *error = NULL;
+  
+  	//printf("%d!\n",strlen(user1));
+  	//printf("%s!\n",user1);
+  	u = converttostring(gtk_entry_get_text(GTK_ENTRY(user)));
+
+  	strcpy(user1,u);
+  	strcat(user1,"\n");
+  	if(l == 0)
+  	{
+  		write(meusocket, user1, strlen(user1));
+
+		read(meusocket, msgreceb, 4096);
+  	}
+  	if( l == 0)
+  	{
+  		l++;
+  	}
+
+	for(i=0;msg[i] != '\0';i++)
+    {
+    	//msgreceb[i] = 0;
+    	msg[i] = 0;
+    	msgenviada[i] = 0;
+    	msgfinal[i] = 0;
+    }
+
+    msg1 = converttostring(gtk_entry_get_text(GTK_ENTRY(entrada)));
+	strcpy(msgenviada,msg1);
+	strcat(msgenviada,"\n");
+
+
+    for(i=0;user1[i] != '\n';i++)
+    {
+    	msg[i] = user1[i];
+    }
+    msg[i] = ':';
+    i++;
+    msg[i] = '\n';
+    for(j=0;msgenviada[j] != '\n';j++)
+    {
+    	msg[i] = msgenviada[j];
+    	i++;
+    }
+    for(j=i;j < strlen(msg);j++)
+   	{
+   	 	msg[j] = ' ';
+   	}
+    msg[i] = '\n';
+
+    
+   	for(i=0;i<strlen(msg);i++)
+  	{
+  		if(msg[i] != ' ')
+  		{
+  			if(msg[i] == '\n') msg[i] = '#';
+  			msgfinal[i] = msg[i];
+  		}
+   	 	
+    }
+  	
+	write(meusocket,msgfinal,strlen(msgfinal));
 	
+
+  	//pt.2
+
   	read(meusocket, msgreceb, 4096);
 
-  	printf(" Mensagem recebida: %s\n", msgreceb);
 
-  	msg = msgreceb;
+  	for(i=0;i < strlen(msgant);i++)
+  	{
+   	 	if(msgant[i] == '#') 
+   	 	{
+   	 		msgant[i] = '\n';
+   	 	}  		
+   	}
+   	//printf("(/%s/)\n",msgant);
+	for(i=0;i< ult;i++)
+  	{
+   	 	if(msgreceb[i] == '#') 
+   	 	{
+   	 		msgreceb[i] = '\n';
+   	 		ult = i;
+   	 		//msgant[i] = '\n';
+   	 	}  		
+   	}
+   	p=0;
+   	//printf("(////%s////)\n",msgreceb);
+	//printf("Mensagem recebida:  %s \n Mensagem anterior : %s \n", msgreceb, msgant);
+	for(i=0; i < ult;i++)
+  	{
+  		if(msgreceb[i] != msgant[i])
+  		{
+  			//printf("satzu\n");
+  			newmsg[p] = msgreceb[i];
+  			//printf("(%c)",newmsg[j]);
+  			p++;
+  			//printf("< %c >", newmsg[i]);
+  		}
+  	}
+  	printf("(%s)\n",newmsg);
+  	for(j=i;j < strlen(newmsg);j++)
+   	{
+   	 	newmsg[j] = ' ';
+   	}
+   	for(j=0;j < strlen(newmsg);j++)
+   	{
+   	 	if(newmsg[j] != ' ')
+   	 	{
+   	 		newmsgfinal[j] = newmsg[j];
+   	 		
+   	 	}
+   	}
+  	strcat(newmsgfinal,"\n");
+  	
+	mensagem = g_convert(newmsgfinal, -1, "UTF-8", "ISO-8859-1", &bytes_read, &bytes_written, &error);
+  	gtk_text_buffer_get_iter_at_line_index(saida,&inicio,lcount,0);
+  	gtk_text_buffer_insert(saida,&inicio,mensagem,-1);
 
-  	gtk_text_buffer_get_iter_at_line(saida,&inicio,i);
-  	gtk_text_buffer_insert(saida,&inicio,msgreceb,-1);
+  	for(i=0;i < 4096;i++)
+  	{
+  		msgant[i] = msgreceb[i];
+  		//	printf("<%c>", msgant[i]);	
+  	}
+  	
+  	for(i=0;i < 4096;i++)
+  	{
+  		newmsg[j] = 0;
+  		newmsgfinal[i] = 0;
+  		//	printf("<%c>", msgant[i]);	
+  	}
+  	i=j=ult=p=0;
+  	//printf("%s\n",newmsg);
+  	//
+		//printf(" %s\n", msgreceb);//printa mensagem recebid
 }
 
-void str_echo(int meusocket)
-{
-	ssize_t n;
-	char buf[4069];
-
-	again:
-		while( (n = read(meusocket, buf, 4096)) > 0)
-		{
-			write(meusocket, buf, n);
-		}
-		if(n < 0 && errno == EINTR)
-			goto again;
-		else if(n<0)
-		exit(1);
-		//break;
-		//erry_sys("str_echo: read error");
-}
-
-char imprimir( GtkWidget *widget,GtkWidget *entrada)
+char imprimir(GtkWidget *widget,GtkWidget *entrada)
 {
 	str_cli(stdin,meusocket,widget,entrada);
   	return 0;
@@ -73,41 +189,61 @@ char imprimir( GtkWidget *widget,GtkWidget *entrada)
 
 void clique1( GtkWidget *widget,gpointer data)
 {
+	GtkWidget *dialog_user;
 	GtkWidget *inside;
 	GtkWidget *janela1;
 	GtkWidget *box2;
 	GtkWidget *box3;
 	GtkWidget *texto;
 	GtkWidget *send;
-	gchar *this;
+	GtkAdjustment *hj;
+	//GtkIMContext * emo;
+	//GtkAdjustment *
+	// GtkDialogFlags flags = GTK_DIALOG_MODAL|GTK_DIALOG_DESTROY_WITH_PARENT;
+	// const gchar *this;
+	// GtkWidget *label_user;
 
-	janela1 = gtk_window_new (GTK_WINDOW_TOPLEVEL); // cria janela
+	janela1 = gtk_window_new(GTK_WINDOW_TOPLEVEL); // cria janela
 	gtk_container_set_border_width(GTK_CONTAINER(janela1),200);//ajuste de altura e largura
 	gtk_window_set_title(GTK_WINDOW(janela1),"Sala"); // nome da janela
 
 	box2 = gtk_vbox_new(FALSE,0);
 	gtk_container_add(GTK_CONTAINER(janela1),box2);
 
+	// dialog_user = gtk_dialog_new_with_buttons("Go!",janela1,flags,_("_OK"),_("_Cancel"),NULL);
+	// gtk_box_pack_start(GTK_BOX(box2),dialog_user,FALSE,FALSE,FALSE,0);
+
 	inside = gtk_scrolled_window_new(NULL,NULL);
+	//hj = gtk_scrolled_window_get_hadjustment(GTK_SCROLLED_WINDOW(inside));
+	/*gtk_adjustment_set_value(hj,hj->upper);
+	gtk_scrolled_window_set_hadjustment(GTK_SCROLLED_WINDOW(inside),hj);*/
 	gtk_box_pack_start(GTK_BOX(box2),inside,FALSE,FALSE,0);
 
 	view = gtk_text_view_new();//cria um novo coiso com proprieades de texto
 
 	saida=gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));// retorna o buffer do view
-	gtk_widget_set_size_request(view,500,100);
+	gtk_widget_set_size_request(view,500,100); 	
+	//gtk_text_view_get_hadjustment (GtkTextView *text_view);
 	//gtk_editable_set_editable(GTK_EDITABLE(view),FALSE);
 	//gtk_text_mark_new(NULL,0);
 	gtk_container_add(GTK_CONTAINER(inside),view); //view dentro do scrolled window
 
+	box3 = gtk_hbox_new(FALSE,0);
+	gtk_box_pack_start(GTK_BOX(box2),box3,FALSE,FALSE,0);
+
 	texto = gtk_entry_new();// para entrada de texto
-	gtk_entry_set_max_length(GTK_ENTRY(texto),180);//tamanho do texto
-	g_signal_connect(texto,"activate",G_CALLBACK(imprimir),texto);//chama uma função que vai imprimir o texto
-	gtk_box_pack_start(GTK_BOX(box2),texto,FALSE,FALSE,0);//coloca na caixa
+	gtk_entry_set_max_length(GTK_ENTRY(texto),1000);//tamanho do texto
+	//g_signal_connect(texto,"activate",G_CALLBACK(imprimir),texto);//chama uma função que vai imprimir o texto
+	gtk_box_pack_start(GTK_BOX(box3),texto,FALSE,FALSE,0);//coloca na caixa
+
+	//emo = gtk_im_context_simple_new();
+	//gtk_im_context_simple_add_table (GTK_IM_CONTEXT_SIMPLE(emo), guint16 *data,GTK_MAX_COMPOSE_LEN,gint n_seqs);
+
+	//insert_emoji(); 
 
 	send = gtk_button_new_with_label("Enviar");
-	gtk_widget_set_size_request(send,5,35);
-	//g_signal_connect(G_OBJECT (send),"clicked",G_CALLBACK(clean),NULL);// função
-	gtk_box_pack_start(GTK_BOX(box2),send,FALSE,FALSE,0);
+	g_signal_connect(G_OBJECT(send),"clicked",G_CALLBACK(imprimir),texto);// função
+	gtk_box_pack_start(GTK_BOX(box3),send,FALSE,FALSE,0);
 	
   	gtk_widget_show_all(janela1);
 }
@@ -136,10 +272,10 @@ int main(int  argc, char *argv[])
 
 	servaddr.sin_family = AF_INET; // familia do socket
 
-	servaddr.sin_port = htons(9502); // porta do socket
+	servaddr.sin_port = htons(9503); // porta do socket
 
 	inet_pton(AF_INET, argv[1], &servaddr.sin_addr); // 
-
+	
 	connect(meusocket, (SA *) &servaddr, sizeof(servaddr)); //conetcando cliente em determidado socket,
 															// usando o valore de retorno do socket, a suct do socket e o tamanho 
 															// da struct severaddr 
@@ -164,29 +300,29 @@ int main(int  argc, char *argv[])
 	boxh1 = gtk_hbox_new(FALSE,0);
 	gtk_box_pack_start(GTK_BOX(boxv1),boxh1,TRUE,TRUE,0);
 
-	boxh2 = gtk_hbox_new(FALSE,0);
+	/*boxh2 = gtk_hbox_new(FALSE,0);
 	gtk_box_pack_start(GTK_BOX(boxv1),boxh2,TRUE,TRUE,0);
-
 	boxh3 = gtk_hbox_new(FALSE,0);
 	gtk_box_pack_start(GTK_BOX(boxv1),boxh3,TRUE,TRUE,0);
-
 	boxh4 = gtk_hbox_new(FALSE,0);
 	gtk_box_pack_start(GTK_BOX(boxv1),boxh4,TRUE,TRUE,0);
-
 	boxh5 = gtk_hbox_new(FALSE,0);
-	gtk_box_pack_start(GTK_BOX(boxv1),boxh5,TRUE,TRUE,0);
+	gtk_box_pack_start(GTK_BOX(boxv1),boxh5,TRUE,TRUE,0);*/
 
-	label = gtk_label_new("Salas Disponiveis");
+	user = gtk_entry_new();// para entrada de texto
+	gtk_entry_set_max_length(GTK_ENTRY(user),20);
+	gtk_box_pack_start(GTK_BOX(boxh1),user,FALSE,FALSE,0);
+
+	label = gtk_label_new("Username:");
 	gtk_box_pack_start(GTK_BOX(boxh6),label,TRUE,TRUE,0);
 
-	botao1 = gtk_button_new_with_label("Sala 1");
+	botao1 = gtk_button_new_with_label("Entrar ?");
 	g_signal_connect(G_OBJECT (botao1),"clicked",G_CALLBACK(clique1),NULL);// função
 	gtk_box_pack_start(GTK_BOX(boxh1),botao1,FALSE,FALSE,0);// coloca botao na caixa
 	
-	botao2 = gtk_button_new_with_label("Sala 2");
+	/*botao2 = gtk_button_new_with_label("Sala 2");
 	g_signal_connect(G_OBJECT (botao2),"clicked",G_CALLBACK(clique1),NULL);// função
 	gtk_box_pack_start(GTK_BOX(boxh2),botao2,FALSE,FALSE,0);
-
 	botao3 = gtk_button_new_with_label("Sala 3");
 	g_signal_connect(G_OBJECT (botao3),"clicked",G_CALLBACK(clique1),NULL);// função
 	gtk_box_pack_start(GTK_BOX(boxh3),botao3,FALSE,FALSE,0);
@@ -194,10 +330,9 @@ int main(int  argc, char *argv[])
 	botao4 = gtk_button_new_with_label("Sala 4");
 	g_signal_connect(G_OBJECT (botao4),"clicked",G_CALLBACK(clique1),NULL);// função
 	gtk_box_pack_start(GTK_BOX(boxh4),botao4,FALSE,FALSE,0);
-
 	botao5 = gtk_button_new_with_label("Sala 5");
 	g_signal_connect(G_OBJECT (botao5),"clicked",G_CALLBACK(clique1),NULL);// função
-	gtk_box_pack_start(GTK_BOX(boxh5),botao5,FALSE,FALSE,0);
+	gtk_box_pack_start(GTK_BOX(boxh5),botao5,FALSE,FALSE,0);*/
 
 	gtk_widget_show_all(fixed);//mostra a box
 	gtk_widget_show_all(janela);//mostra a janela
